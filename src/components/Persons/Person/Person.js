40e396a0bff5e933829fc84fd6cdf6c11b720319
Manjado <michalalchimowicz@gmail.com>
2017-12-13 08:24:31 +0100

    ref

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -17,6 +17,7 @@
 //from stateless to stateful
 
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import classes from './Person.css';
 import withClass from '../../../hoc/withClassOther';
@@ -34,6 +35,9 @@ class Person extends Component {
 
   componentDidMount() {
     console.log('[Person.js] Inside componentDidMount()');
+    if (this.props.position === 0) {
+    	this.inputElement.focus();
+    }
   }
 
 	render() {
@@ -43,10 +47,23 @@ class Person extends Component {
 			<Aux>
 				<p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
 				<p>{this.props.children}</p>
-				<input onChange={this.props.changed} value={this.props.name}/>
+				<input 
+					ref={(inp) => { this.inputElement = inp }}
+					onChange={this.props.changed} 
+					value={this.props.name}/>
 			</Aux>
 		)
 	}
 }
+
+Person.propTypes = {
+	click: PropTypes.func,
+	name: PropTypes.string,
+	age: PropTypes.number,
+	changed: PropTypes.func
+};
+
+
+
 
 export default withClass(Person,classes.Person);
