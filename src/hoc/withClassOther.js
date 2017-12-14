4d07d5a51b9fdba8b3c60f1fadc5a3ea6b31fddb
Manjado <michalalchimowicz@gmail.com>
2017-12-14 06:31:12 +0100

    the end

---
+++ src/hoc/withClassOther.js
@@ -0,0 +1,28 @@
+// import React from 'react';
+
+// const withClass = (WrappedComponent, className) => {
+// 	return (props) => (
+// 		<div className={className}>
+// 			<WrappedComponent {...props}/>
+// 		</div>
+// 	)
+// }
+
+//from stateless to stateful
+
+
+import React, { Component } from 'react';
+
+const withClass = (WrappedComponent,className) => {
+	return class extends Component {
+		render () {
+			return(
+				<div className={className}>
+					<WrappedComponent {...this.props} />
+				</div>
+			)
+		}
+	}
+}
+
+export default withClass;
